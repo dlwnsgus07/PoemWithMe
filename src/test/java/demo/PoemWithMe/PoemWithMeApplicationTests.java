a04fbe97a7c86a57fbf6e3aed846f094c32479e4
JunHyeon <dlwnsgus07@naver.com>
2023-03-21 21:30:32 +0900

    add: main Test

---
+++ src/test/java/demo/PoemWithMe/PoemWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.PoemWithMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoemWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
