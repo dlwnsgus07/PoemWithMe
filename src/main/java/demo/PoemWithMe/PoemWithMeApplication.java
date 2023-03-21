cf3e3a95ff16052caaedb900a36fb3d861b56ab8
JunHyeon <dlwnsgus07@naver.com>
2023-03-21 21:30:01 +0900

    add: MemberRepository

---
+++ src/main/java/demo/PoemWithMe/PoemWithMeApplication.java
@@ -0,0 +1,13 @@
+package demo.PoemWithMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoemWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoemWithMeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/demo/PoemWithMe/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package demo.PoemWithMe.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+}
