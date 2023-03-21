2309578c17a8813b19a2ddd511f159934b6e064d
JunHyeon <dlwnsgus07@naver.com>
2023-03-21 21:26:43 +0900

    add: JWT Configure bean

---
+++ src/main/java/demo/PoemWithMe/auth/JWTHandler.java
@@ -0,0 +1,50 @@
+package demo.PoemWithMe.auth;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Slf4j
+@Component
+public class JWTHandler {
+    private final String type = "Bearer";
+
+    public String CreateToken(String encodedKey, String subject, Long maxAgeSeconds) {
+        Date now = new Date();
+        return type + Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + maxAgeSeconds * 1000L))
+                .signWith(SignatureAlgorithm.HS256, encodedKey)
+                .compact();
+    }
+
+    public String extractSubject(String encodedKey, String token) {
+        return parse(encodedKey, token).getBody().getSubject();
+    }
+
+    public boolean validate(String encodedKey, String token) {
+        try {
+            parse(encodedKey, token);
+            return true;
+        } catch (JwtException e) {
+            log.error("토큰 오류 발생" + e.getMessage());
+            return false;
+        }
+    }
+
+    private Jws<Claims> parse(String key, String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(unType(token));
+    }
+
+    private String unType(String token) {
+        return token.substring(type.length());
+    }
+
+
+}
