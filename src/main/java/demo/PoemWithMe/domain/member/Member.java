556439ec9611c180680486deb1b2044a466d72f9
JunHyeon <dlwnsgus07@naver.com>
2023-03-22 17:22:25 +0900

    fix : add restrict annotation into email

--- src/main/java/demo/PoemWithMe/domain/member/Member.java
+++ src/main/java/demo/PoemWithMe/domain/member/Member.java
@@ -2,6 +2,7 @@ package demo.PoemWithMe.domain.member;
 
 import demo.PoemWithMe.domain.comment.Comment;
 import demo.PoemWithMe.domain.poem.Poem;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 
 import java.util.List;
@@ -11,6 +12,8 @@ public class Member {
     private Long id;
     private String name;
     private String password;
+    @Email
+    private String email;
     private String nickName;
     private List<Poem> likePoemList;
     private List<Comment> comments;
