56381386ca829d450adb2f47acf7964a60c625ce
JunHyeon <dlwnsgus07@naver.com>
2023-04-01 21:48:27 +0900

    fix : enum타입이 뜻과 key를 갖도록 수정

--- src/main/java/demo/PoemWithMe/domain/member/ROLE.java
+++ src/main/java/demo/PoemWithMe/domain/member/ROLE.java
@@ -1,5 +1,12 @@
 package demo.PoemWithMe.domain.member;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ROLE {
-   WRITER, PUBLISHER, VIP, ADMIN
+    WRITER("WRITER", 1), PUBLISHER("PUBLISHER", 2), VIP("VIP", 3), ADMIN("ADMIN", 4);
+    private final String title;
+    private final int key;
 }
