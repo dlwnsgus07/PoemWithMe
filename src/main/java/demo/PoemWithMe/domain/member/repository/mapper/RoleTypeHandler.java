a929c6fa9c2434a0de9aa4c9df49ca9f818cbfdf
JunHyeon <dlwnsgus07@naver.com>
2023-04-01 21:48:01 +0900

    feat: Enum타입에 대한 데이터베이스 TypeHandler추가

---
+++ src/main/java/demo/PoemWithMe/domain/member/repository/mapper/RoleTypeHandler.java
@@ -0,0 +1,48 @@
+package demo.PoemWithMe.domain.member.repository.mapper;
+
+import demo.PoemWithMe.domain.member.ROLE;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedTypes;
+import org.apache.ibatis.type.TypeHandler;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@MappedTypes(ROLE.class)
+public class RoleTypeHandler implements TypeHandler<ROLE> {
+    @Override
+    public void setParameter(PreparedStatement ps, int i, ROLE parameter, JdbcType jdbcType) throws SQLException {
+        ps.setString(i, parameter.getTitle());
+    }
+
+    @Override
+    public ROLE getResult(ResultSet rs, String columnName) throws SQLException {
+        int roleKey = rs.getInt(columnName);
+        return getRole(roleKey);
+    }
+
+    @Override
+    public ROLE getResult(ResultSet rs, int columnIndex) throws SQLException {
+        int roleKey = rs.getInt(columnIndex);
+        return getRole(roleKey);
+    }
+
+    @Override
+    public ROLE getResult(CallableStatement cs, int columnIndex) throws SQLException {
+        int roleKey = cs.getInt(columnIndex);
+        return getRole(roleKey);
+    }
+
+    private ROLE getRole(int roleKey) {
+        return switch (roleKey) {
+            case 1 -> ROLE.WRITER;
+            case 2 -> ROLE.PUBLISHER;
+            case 3 -> ROLE.VIP;
+            case 4 -> ROLE.ADMIN;
+            default -> null;
+        };
+    }
+}
+
